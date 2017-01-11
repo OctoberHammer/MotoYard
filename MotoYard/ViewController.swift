d8633a01656495892cd065b63161fa2e2933507b
OctoberHammer <octoberhammer@gmail.com>
2017-01-11 09:51:55 +0200

    Initial Commit

---
+++ MotoYard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MotoYard
+//
+//  Created by October Hammer on 1/11/17.
+//  Copyright © 2017 Ocotober Hammer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
